d104967a6045eaa74fb5da6a529378bd1aea513e
Davis Muro <davis.ray.muro@gmail.com>
2024-09-28 00:25:01 -0700

    A bit of cleanup

--- parallel_sum.cc
+++ parallel_sum.cc
@@ -1,35 +1,34 @@
 /// Parallel Sum - Work-efficient algorithm implementation.
 /// Based on the Wikipedia algorithm explanation & explanations from
-/// CPSC 5600 F24 (Taught by Braxton Cuneo)
+/// CPSC 5600 F24
 ///
 /// Note: This code could probably be shortened quite a bit if one decides to
-/// use Vectors & std::inclusive_scan for the sequentielly portions of the
-/// code... But, what's the fun of that ?
-#include <chrono>
+/// use Vectors & std::inclusive_scan for the sequentiel portions of the
+/// code...
 #include <iostream>
 #include <omp.h>
 #include <stdlib.h>
-
+#ifdef TIMING
+#include <chrono>
 using std::chrono::duration_cast;
 using std::chrono::steady_clock;
 using TimePoint = std::chrono::steady_clock::time_point;
 using TimeSpan = std::chrono::duration<double>;
+#endif
 
 /// Generates a array containing random numbers in the range 1 to 100
-int *random_array(int count) {
-  int *result = new int[count];
-  for (int i = 0; i < count; i++) {
-    result[i] = rand() % 100 + 1;
-  }
+int *random_array(int count);
 
-  return result;
-}
+/// Prints array to stdout without newline
+void print_array(int *arr, int size);
 
-void print_array(int *arr, int size) {
-  for (int i = 0; i < size; i++) {
-    std::cout << arr[i] << ' ';
-  }
-}
+/// Generates intermediate Sequence `z` from the source array. z0 = x0 + x1,
+/// z1 = x2 + x3, ...
+int *calculate_sequence_z(int *x, int sourceArrSize);
+
+/// Generates sequence w by calculating the prefix sum of intermediate sequence
+/// z
+int *calculate_sequence_w(int *z, int sourceArrSize);
 
 int main(int argc, char *argv[]) {
   if (argc != 3) {
@@ -48,53 +47,10 @@ int main(int argc, char *argv[]) {
   print_array(x, arraySize);
   std::cout << "\n\n";
 
-  // Step 1: Calculate itermediate sequence z; z_0 = x_0 + x_1, z_1 = x_2 +
-  // x_3...
-  int *z = new int[arraySize / 2];
+  int *z = calculate_sequence_z(x, arraySize);
+  int *w = calculate_sequence_w(z, arraySize);
 
-  std::cout << "Calculating sequence z in parallel\n";
-
-  // Start Time 0
-  TimePoint st0 = steady_clock::now();
-
-#pragma omp parallel for
-  for (int i = 0; i < arraySize / 2; i++) {
-    z[i] = x[2 * i] + x[2 * i + 1];
-  }
-
-  // End Time 0
-  TimePoint et0 = steady_clock::now();
-  // Time Taken 0
-  TimeSpan tt0 = duration_cast<TimeSpan>(et0 - st0);
-
-  std::cout << "Sequence z: ";
-  print_array(z, arraySize / 2);
-  std::cout << "\n\nTime taken to calculate: " << tt0.count() << " seconds\n\n";
-
-  // Step 2: Calculate the prefix sum of intermediate sequence z and assign to
-  // sequence w
-  int *w = new int[arraySize / 2];
-  w[0] = z[0];
-
-  std::cout << "Calculating sequence w in sequentially\n";
-
-  // Start Time 1
-  TimePoint st1 = steady_clock::now();
-
-  for (int i = 1; i < arraySize / 2; i++) {
-    w[i] = w[i - 1] + z[i];
-  }
-
-  // End Time 1
-  TimePoint et1 = steady_clock::now();
-  // Time Taken 1
-  TimeSpan tt1 = duration_cast<TimeSpan>(et1 - st1);
-
-  std::cout << "Sequence w: ";
-  print_array(w, arraySize / 2);
-  std::cout << "\n\nTime taken to calculate: " << tt1.count() << " seconds\n\n";
-
-  // Step 3: Calculate the final sequence y
+  // Calculate the prefix sum array using the intermediate sequences z & w
   int *y = new int[arraySize];
   y[0] = x[0];
   y[1] = z[0];
@@ -103,8 +59,11 @@ int main(int argc, char *argv[]) {
 
   std::cout << "Calculating sequence y in parallel\n";
 
+#ifdef TIMING
   // Start Time 2
   TimePoint st2 = steady_clock::now();
+#endif
+
 #pragma omp parallel for
   for (int i = 2; i < arraySize; i++) {
     // If even calculate value as sum of previous value and position i in x
@@ -115,17 +74,104 @@ int main(int argc, char *argv[]) {
     }
   }
 
+#ifdef TIMING
   // End Time 2
   TimePoint et2 = steady_clock::now();
   // Time Taken 0
   TimeSpan tt2 = duration_cast<TimeSpan>(et2 - st2);
+#endif
 
   std::cout << "Prefix Sum Array (y): ";
   print_array(y, arraySize);
-  std::cout << "\n\nTime taken to calculate: " << tt2.count() << " seconds\n\n";
+  std::cout << "\n";
+
+#ifdef TIMING
+  std::cout << "\nTime taken to calculate: " << tt2.count() << " seconds\n\n";
+#endif
 
   delete[] x;
   delete[] y;
 
   return 0;
+}
+
+int *random_array(int count) {
+  int *result = new int[count];
+  for (int i = 0; i < count; i++) {
+    result[i] = rand() % 100 + 1;
+  }
+
+  return result;
+}
+
+void print_array(int *arr, int size) {
+  for (int i = 0; i < size; i++) {
+    std::cout << arr[i] << ' ';
+  }
+}
+
+int *calculate_sequence_z(int *x, int sourceArrSize) {
+  int *result = new int[sourceArrSize / 2];
+
+  std::cout << "Calculating sequence z in parallel\n";
+
+#ifdef TIMING
+  // Start Time 0
+  TimePoint st0 = steady_clock::now();
+#endif
+
+#pragma omp parallel for
+  for (int i = 0; i < sourceArrSize / 2; i++) {
+    result[i] = x[2 * i] + x[2 * i + 1];
+  }
+
+#ifdef TIMING
+  // End Time 0
+  TimePoint et0 = steady_clock::now();
+  // Time Taken 0
+  TimeSpan tt0 = duration_cast<TimeSpan>(et0 - st0);
+#endif
+
+  std::cout << "Sequence z: ";
+  print_array(result, sourceArrSize / 2);
+  std::cout << "\n";
+
+#ifdef TIMING
+  std::cout << "\nTime taken to calculate: " << tt0.count() << " seconds\n\n";
+#endif
+
+  return result;
+}
+
+int *calculate_sequence_w(int *z, int sourceArrSize) {
+  int *result = new int[sourceArrSize / 2];
+  result[0] = z[0];
+
+  std::cout << "Calculating sequence w in sequentially\n";
+
+#ifdef TIMING
+  // Start Time 1
+  TimePoint st1 = steady_clock::now();
+#endif
+
+  for (int i = 1; i < sourceArrSize / 2; i++) {
+    result[i] = result[i - 1] + z[i];
+  }
+
+#ifdef TIMING
+  // End Time 1
+  TimePoint et1 = steady_clock::now();
+  // Time Taken 1
+  TimeSpan tt1 = duration_cast<TimeSpan>(et1 - st1);
+#endif
+
+  std::cout << "Sequence w: ";
+  print_array(result, sourceArrSize / 2);
+  std::cout << "\n";
+
+#ifdef TIMING
+  std::cout << "\nTime taken to calculate: " << tt1.count() << " seconds\n\n";
+#endif
+
+  return result;
 }
